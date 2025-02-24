5c68733b34fdf08b3c45db872cae2ddbd072c0ed
DominikDO98 <dominiklas13@wp.pl>
2025-02-24 19:10:46 +0100

    Feat: Added middlewares and basic endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
+import cookieParser from "cookie-parser";
 import express, { Request, Response } from "express";
+import { Authentication } from "utils/authentication";
 
 const app = express();
 
-app.get("/", (_req: Request, res: Response) => {
-  res.json({ hello: "world" });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  console.log(req.query, req.cookies.State);
+
+  if (
+    req.query.state &&
+    req.cookies.State &&
+    req.query.state === req.cookies.State
+  )
+    res.sendStatus(200);
+  if (
+    !req.query.state ||
+    !req.cookies.State ||
+    req.query.state !== req.cookies.State
+  )
+    res.sendStatus(403);
+});
+app.get("/login", (_req: Request, res: Response) => {
+  new Authentication().redirectUser(res);
 });
 
 app.listen(3000, "localhost", () => {
