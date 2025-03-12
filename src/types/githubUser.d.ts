5457cec26a78b1b66cf62ba94fba4a9ba40819d8
DominikDO98 <dominiklas13@wp.pl>
2025-03-12 09:36:44 +0100

    Feat: Created file for GithubUser types

---
+++ src/types/githubUser.d.ts
@@ -0,0 +1,11 @@
+export interface IGithubUser {
+  id: number;
+  login: string;
+  avatar_url: string;
+}
+
+export interface IGithubUserDO {
+  id: number;
+  login: string;
+  avatarUrl: string;
+}
