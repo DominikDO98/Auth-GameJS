bc20f3c44e6ce1fc4ce87719dc7599af9419f718
DominikDO98 <dominiklas13@wp.pl>
2025-05-06 10:50:10 +0200

    Feat: Moved middlewares to separate class

---
+++ src/middleware/index.ts
@@ -0,0 +1,17 @@
+import cookieParser from "cookie-parser";
+import express, { Express } from "express";
+import cors from "cors";
+
+export class Middleware {
+  private _app;
+  constructor(app: Express) {
+    this._app = app;
+  }
+
+  initMiddleware() {
+    this._app.use(express.urlencoded({ extended: true }));
+    this._app.use(express.json());
+    this._app.use(cookieParser());
+    this._app.use(cors({ origin: process.env.frontendURL, credentials: true }));
+  }
+}
