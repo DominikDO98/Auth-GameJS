3ab1590be6c75872773154419726a8714e8cc1be
DominikDO98 <dominiklas13@wp.pl>
2025-05-06 10:50:10 +0200

    Feat: Moved middlewares to separate class

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-import cookieParser from "cookie-parser";
-import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import { Middleware } from "middleware";
 import { MainRouter } from "routers";
 class App {
   app: express.Express;
   router: MainRouter;
+  middleware: Middleware;
+
   constructor() {
     this.app = express();
     this.router = new MainRouter(this.app);
+    this.middleware = new Middleware(this.app);
   }
 
   init() {
-    this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(express.json());
-    this.app.use(cookieParser());
-    this.app.use(cors({ origin: process.env.frontendURL, credentials: true }));
+    this.middleware.initMiddleware();
     this.router.initRoutes();
     this.app.listen(3000, "localhost", () => {
       console.log("Server is running on port 3000...");
